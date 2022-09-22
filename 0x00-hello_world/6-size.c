3d409d9066680632c797f660a0f0565add035713
Bukola <smartesther02@gmail.com>
2022-09-22 16:31:47 -0700

    Size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -16,5 +16,7 @@ int main(void)
 	printf("Size of an int: %d byte(s)\n", sizeof(b));
 	printf("Size of a long int: %d byte(s)\n", sizeof(c));
 	printf("Size of a long long int: %d byte(s)\n", sizeof(p));
-	pintrf("Size of a float: %d byte(s)\n", sizeof(q));
+	printf("Size of a float: %d byte(s)\n", sizeof(q));
+
+	return (0);
 }
