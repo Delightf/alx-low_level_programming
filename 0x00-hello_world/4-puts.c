8c819bfca64bdeb65588faaaf09ef467ba720c62
Bukola <smartesther02@gmail.com>
2022-09-22 15:46:52 -0700

    Puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - TO print using PUTS
+ *
+ * Return: Will be 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0);
+}
